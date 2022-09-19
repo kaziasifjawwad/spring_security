5058f25196b89e386469c043479fcbbb5c25e58b
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-09-19 14:57:56 +0600

    exception handled

--- src/main/java/com/brainstation/spring_security/initializer/RoleInitializer.java
+++ src/main/java/com/brainstation/spring_security/initializer/RoleInitializer.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @RequiredArgsConstructor
 public class RoleInitializer implements CommandLineRunner {
   private final RoleService roleService;
--- src/main/java/com/brainstation/spring_security/security/MyUserDetailsService.java
+++ src/main/java/com/brainstation/spring_security/security/MyUserDetailsService.java
@@ -1,11 +1,14 @@
 package com.brainstation.spring_security.security;
 
 import com.brainstation.spring_security.models.MyUserDetails;
+import com.brainstation.spring_security.models.User;
 import com.brainstation.spring_security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public class MyUserDetailsService implements UserDetailsService {
 
@@ -20,8 +23,10 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user  = this.userRepository.findByEmail(username);
+        if(user.isEmpty()) throw new UsernameNotFoundException("User not found");
         return new MyUserDetails(
-                this.userRepository.findByEmail(username).get()
+                user.get()
         );
     }
 }
